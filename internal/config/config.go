6e4a188e75209604046390d32b26e14281171472
Tim Dohm <tim.dohm@thirdfort.com>
2024-05-20 14:06:30 +0100

    feat: :sparkles: change config to API

--- internal/config/config.go
+++ internal/config/config.go
@@ -33,7 +33,7 @@ type Config struct {
 	Service struct {
 		Port    int    `env:"PORT" env-default:"6060"`
 		Address string `env:"ADDRESS" env-default:"0.0.0.0"`
-	} `yaml:"service" env-prefix:"CONAPI_"`
+	} `yaml:"service" env-prefix:"API_"`
 	Postgres struct {
 		User        string `env:"USER" yaml:"user" env-default:"postgres"`
 		Password    string `env:"PASSWORD" yaml:"password" env-default:"postgres"`
@@ -41,14 +41,14 @@ type Config struct {
 		Database    string `env:"NAME" yaml:"name" env-default:"consumer-api"`
 		Port        string `env:"PORT" yaml:"port" env-default:"5432"`
 		EnableDebug bool   `env:"DEBUG" yaml:"debug" env-default:"false"`
-	} `yaml:"postgres" env-prefix:"CONAPI_POSTGRES_"`
+	} `yaml:"postgres" env-prefix:"API_POSTGRES_"`
 	Clients struct {
 		Onfido struct {
 			AppID struct {
 				Android string `env:"ANDROID" yaml:"android" env-default:"app.thirdfort.android.sandbox"`
 				Ios     string `env:"IOS" yaml:"ios" env-default:"app.thirdfort.ios.sandbox"`
 			} `env:"APPID_" yaml:"app_id"`
-		} `yaml:"onfido" env-prefix:"CONAPI_ONFIDO_"`
+		} `yaml:"onfido" env-prefix:"API_ONFIDO_"`
 		PlatformAPI struct {
 			Aud       string `env:"AUD" env-default:"https://api-t.thirdfort.io/v2/"`
 			Url       string `env:"URL" env-default:"https://sandbox.api.thirdfort.io"`
@@ -61,22 +61,22 @@ type Config struct {
 				Consumer []string `env:"ISSUERS_CONSUMER" env-default:"[https://securetoken.google.com/sandbox-thirdfort-app]"`
 			}
 		} `yaml:"jwt" env-prefix:"JWT_"`
-	} `yaml:"clients" env-prefix:"CONAPI_"`
+	} `yaml:"clients" env-prefix:"API_"`
 	Otel struct {
 		Enabled         bool          `env:"ENABLED" env-default:"false"`
 		PushInterval    time.Duration `env:"PUSH_INTERVAL" yaml:"push_interval" env-default:"10000ms"`
 		RuntimeInterval time.Duration `env:"RUNTIME_INTERVAL" yaml:"runtime_interval" env-default:"15000ms"`
 		SamplingRatio   float64       `env:"SAMPLING_RATIO" yaml:"sampling_ratio" env-default:"1"`
-	} `yaml:"otel" env-prefix:"CONAPI_OTEL_"`
+	} `yaml:"otel" env-prefix:"API_OTEL_"`
 	Sentry struct {
 		Dsn         string      `env:"DSN" yaml:"dsn" env-default:""`
 		Environment Environment `env:"ENV" yaml:"environment" env-default:"local"`
-	} `yaml:"sentry" env-prefix:"CONAPI_SENTRY_"`
+	} `yaml:"sentry" env-prefix:"API_SENTRY_"`
 	PubSub struct {
 		ProjectID string `env:"PROJECT_ID" env-default:"local"`
 		Timeout   int    `env:"TIMEOUT" env-default:"500"`
 		Topic     string `env:"TOPIC" env-default:"consumer-api"`
-	} `yaml:"pubsub" env-prefix:"CONAPI_PUBSUB_"`
+	} `yaml:"pubsub" env-prefix:"API_PUBSUB_"`
 }
 
 func New() (*Config, error) {
